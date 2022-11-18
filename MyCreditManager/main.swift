69b353aa4bfce811e38a7b0ce5ccc860a4920ebf
taek0622 <min4644@gmail.com>
2022-11-19 00:09:46 +0900

    Feat:sparkles:: Add combiningFunction
    - 입력에 따른 처리를 하기 위한 기능 추가

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -10,4 +10,26 @@ import Foundation
 while true {
     print("원하는 기능을 입력해주세요")
     print("1: 학생추가, 2: 학생삭제, 3: 성적추가 (변경), 4: 성적삭제, 5: 평점보기, X: 종료")
+
+    combiningFunction(input: readLine()!)
+}
+
+func combiningFunction(input: String) {
+    switch input {
+    case "1":
+        print("학생추가")
+    case "2":
+        print("학생삭제")
+    case "3":
+        print("성적추가 (변경)")
+    case "4":
+        print("성적삭제")
+    case "5":
+        print("평점보기")
+    case "X":
+        print("종료")
+    default:
+        print("메뉴의 잘못된 입력 처리")
+    }
+}
 }
