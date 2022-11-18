2ea9efb2fcdb81f6641e478de8eaca0a51773b10
taek0622 <min4644@gmail.com>
2022-11-19 00:13:11 +0900

    Feat:sparkles:: Add updateScore function
    - 성적추가 (변경) 기능 추가

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -23,7 +23,7 @@ func combiningFunction(input: String) {
     case "2":
         removeStudent()
     case "3":
-        print("성적추가 (변경)")
+        updateScore()
     case "4":
         print("성적삭제")
     case "5":
@@ -60,6 +60,23 @@ func removeStudent() {
         print("\(name) 학생을 삭제하였습니다.")
     } else {
         print("\(name) 학생을 찾지 못했습니다.")
+    }
+}
+
+func updateScore() {
+    print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.")
+    print("입력예) Mickey Swift A+")
+    print("만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
+
+    let input = readLine()!.split(separator: " ").map { String($0) }
+
+    if input.count != 3 {
+        print("입력이 잘못되엇습니다. 다시 확인해주세요.")
+    } else if students[input[0]] != nil {
+        students[input[0]]?[input[1]] = input[2]
+        print("\(input[0]) 학생의 \(input[1]) 과목이 \(input[2])로 추가(변경)되었습니다.")
+    } else {
+        print("\(input[0]) 학생의 \(input[1]) 과목이 \(input[2])로 추가(변경)되었습니다.")
     }
 }
 
